20fe1b0cc860969a48ca56ce39e4e72710de0a31
1190503 - Diogo Lima <1190503@isep.ipp.pt>
2024-10-11 12:49:05 +0100

    Sequence diagrams finished and Logical View 1.

--- Domain/SystemUser/CreateBackofficeUserDto.cs
+++ Domain/SystemUser/CreateBackofficeUserDto.cs
@@ -1,4 +1,6 @@
-﻿namespace Sem5Pi2425.Domain.SystemUser;
+﻿using Microsoft.Extensions.FileProviders;
+
+namespace Sem5Pi2425.Domain.SystemUser;
 
 public class CreateBackofficeUserDto {
     public string Username { set; get; }
@@ -6,4 +8,5 @@ public class CreateBackofficeUserDto {
     public string FullName { get; set; }
     public string PhoneNumber { get; set; }
     public string Role { get; set; }
+    
 }
