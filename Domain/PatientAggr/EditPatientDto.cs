c33f6d2db6a9974f590ff9331561a75048134e44
1190503 - Diogo Lima <1190503@isep.ipp.pt>
2024-10-19 23:05:47 +0100

    EditPatientDto

--- Domain/PatientAggr/EditPatientDto.cs
+++ Domain/PatientAggr/EditPatientDto.cs
@@ -2,12 +2,15 @@ using System.Collections.Generic;
 
 namespace Sem5Pi2425.Domain.PatientAggr;
 
-public class UpdatePatientDto
+public class EditPatientDto
 {
+    public string Email { get; set; }
     public string FullName { get; set; }
     public string PhoneNumber { get; set; }
-    public string Email { get; set; }
-    public EmergencyContact EmergencyContact { get; set; }
-    public List<string> MedicalConditions { get; set; }
-    public List<string> Allergies { get; set; }
+    public string BirthDate { get; set; }
+    public string Gender { get; set; }
+    public string EmergencyContactFullName { get; set; }
+    public string EmergencyContactEmail { get; set; }
+    public string EmergencyContactPhoneNumber { get; set; }
+    public string MedicalConditions { get; set; }
 }
