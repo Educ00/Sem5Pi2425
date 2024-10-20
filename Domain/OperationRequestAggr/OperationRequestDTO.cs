05518f6924512ff95b2b9bb7a083374d62ab30d9
BiaMorais30 <127674486+biamorais3@users.noreply.github.com>
2024-10-20 20:18:56 +0100

    added operationType to operationRequest domain

--- Domain/OperationRequestAggr/OperationRequestDTO.cs
+++ Domain/OperationRequestAggr/OperationRequestDTO.cs
@@ -1,4 +1,5 @@
 using System;
+using Sem5Pi2425.Domain.OperationTypeAggr;
 using Sem5Pi2425.Domain.PatientAggr;
 using Sem5Pi2425.Domain.SystemUserAggr;
 
@@ -15,17 +16,20 @@ namespace Sem5Pi2425.Domain.OperationRequestAggr
         public User Doctor { get; set; }
         
         public Patient Patient { get; set; }
+        public OperationType OperationType { get; set; }
+
         
-        public OperationRequestDTO(OperationRequest operationRequest) : this(operationRequest.Id, operationRequest.Deadline, operationRequest.Priority, operationRequest.Doctor, operationRequest.Patient) {}
+        public OperationRequestDTO(OperationRequest operationRequest) : this(operationRequest.Id, operationRequest.Deadline, operationRequest.Priority, operationRequest.Doctor, operationRequest.Patient, operationRequest.OperationType) {}
 
         public OperationRequestDTO(OperationRequestId id, DateTime deadline, Priority priority, User doctor,
-            Patient patient)
+            Patient patient, OperationType operationType)
         {
             Id = id;
             Deadline = deadline;
             Priority = priority;
             Doctor = doctor;
             Patient = patient;
+            OperationType = operationType;
         }
 
     }
