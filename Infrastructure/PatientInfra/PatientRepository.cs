9de48fd0cb1ce1ce7bfa54688b7d80137ce1d0df
Eduardo Carreiro <109857329+Educ00@users.noreply.github.com>
2024-10-20 20:42:29 +0100

    Fixed a bug where the user would not load with the associated patient.

--- Infrastructure/PatientInfra/PatientRepository.cs
+++ Infrastructure/PatientInfra/PatientRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Sem5Pi2425.Domain.PatientAggr;
@@ -8,8 +9,31 @@ using Sem5Pi2425.Infrastructure.Shared;
 namespace Sem5Pi2425.Infrastructure.PatientInfra;
 
 public class PatientRepository : BaseRepository<Patient, UserId>, IPatientRepository {
-    public PatientRepository(Sem5Pi2425DbContext context) : base(context.Patients)
-    {
-        
+    public PatientRepository(Sem5Pi2425DbContext context) : base(context.Patients) { }
+
+
+    public new async Task<List<Patient>> GetAllAsync() {
+        return await this.Objs
+            .Include(p => p.User)
+            .ToListAsync();
+    }
+
+    public new async Task<Patient> GetByIdAsync(UserId id) {
+        return await this.Objs
+            .Include(p => p.User)
+            .FirstOrDefaultAsync(x => id.Equals(x.Id));
+    }
+
+    public new async Task<List<Patient>> GetByIdsAsync(List<UserId> ids) {
+        return await this.Objs
+            .Include(p => p.User)
+            .Where(x => ids.Contains(x.Id))
+            .ToListAsync();
+    }
+
+    public async Task<Patient> GetByUsernameAsync(string username) {
+        return await Objs
+            .Include(p => p.User)
+            .FirstOrDefaultAsync(x => x.User.Username.Value.Equals(username));
     }
 }
