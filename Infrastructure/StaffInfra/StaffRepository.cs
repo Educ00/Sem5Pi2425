f0311316fd3f79b9a8d9af6102053f615a08294c
Eduardo Carreiro <109857329+Educ00@users.noreply.github.com>
2024-10-20 23:48:00 +0100

    Fixed staff repository not fetching corresponding staff's user

--- Infrastructure/StaffInfra/StaffRepository.cs
+++ Infrastructure/StaffInfra/StaffRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Sem5Pi2425.Domain.StaffAggr;
@@ -7,7 +8,31 @@ using Sem5Pi2425.Infrastructure.Shared;
 
 namespace Sem5Pi2425.Infrastructure.StaffInfra;
 
-public class StaffRepository : BaseRepository<Staff, UserId>,IStaffRepository {
+public class StaffRepository : BaseRepository<Staff, UserId>, IStaffRepository {
     public StaffRepository(Sem5Pi2425DbContext context) : base(context.Staffs) { }
-    
+
+    public new async Task<List<Staff>> GetAllAsync() {
+        return await this.Objs
+            .Include(p => p.User)
+            .ToListAsync();
+    }
+
+    public new async Task<Staff> GetByIdAsync(UserId id) {
+        return await this.Objs
+            .Include(p => p.User)
+            .FirstOrDefaultAsync(x => id.Equals(x.Id));
+    }
+
+    public new async Task<List<Staff>> GetByIdsAsync(List<UserId> ids) {
+        return await this.Objs
+            .Include(p => p.User)
+            .Where(x => ids.Contains(x.Id))
+            .ToListAsync();
+    }
+
+    public async Task<Staff> GetByUsernameAsync(string username) {
+        return await Objs
+            .Include(p => p.User)
+            .FirstOrDefaultAsync(x => x.User.Username.Value.Equals(username));
+    }
 }
