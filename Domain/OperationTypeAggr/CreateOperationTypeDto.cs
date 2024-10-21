08cae6a89c978eb0190acaf3ae8af6166c6111f4
BiaMorais30 <127674486+biamorais3@users.noreply.github.com>
2024-10-21 23:52:53 +0100

    US 5.1.16 created class CreateOperationTypeDto

---
+++ Domain/OperationTypeAggr/CreateOperationTypeDto.cs
@@ -0,0 +1,22 @@
+namespace Sem5Pi2425.Domain.OperationTypeAggr {
+
+    public class CreateOperationTypeDto {
+        public string Id { get; set; }
+        public string Duration { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string NeededSpecializations { get; set; }
+        public string Active { get; set; }
+        
+        
+        public CreateOperationTypeDto(string id, string duration, string name, string description,
+            string neededSpecializations, string active) {
+            Id = id;
+            Duration = duration;
+            Name = name;
+            Description = description;
+            NeededSpecializations = neededSpecializations;
+            Active = active;
+        }
+    }
+}
