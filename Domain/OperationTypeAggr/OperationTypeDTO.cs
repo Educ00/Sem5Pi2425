0894c8e125907178c9123a2547c342be124a87ea
BiaMorais30 <127674486+biamorais3@users.noreply.github.com>
2024-10-21 21:23:03 +0100

    fixed class OperationTypeDto

--- Domain/OperationTypeAggr/OperationTypeDTO.cs
+++ Domain/OperationTypeAggr/OperationTypeDTO.cs
@@ -1,30 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using Microsoft.AspNetCore.JsonPatch.Operations;
-using Sem5Pi2425.Domain.OperationTypeAggr;
-using OperationType = Microsoft.AspNetCore.JsonPatch.Operations.OperationType;
 
-public class OperationTypeDto
-{
-    public OperationTypeId Id { get; set; }
-    public DateTime Duration { get; set; }
-    public Name Name { get; set; }
-    public Description Description { get; set; }
-    public List<string> NeededSpecializations { get; set; }
-    public Boolean Active { get; set; }
-    
-    public OperationTypeDto(Sem5Pi2425.Domain.OperationTypeAggr.OperationType operationType): this(operationType.Id, operationType.Duration, operationType.Name, operationType.Description, operationType.NeededSpecializations, operationType.Active)
+namespace Sem5Pi2425.Domain.OperationTypeAggr {
+
+    public class OperationTypeDto
     {
-    }
+        public OperationTypeId Id { get; set; }
+        public DateTime Duration { get; set; }
+        public Name Name { get; set; }
+        public Description Description { get; set; }
+        public List<string> NeededSpecializations { get; set; }
+        public Boolean Active { get; set; }
 
-
-    public OperationTypeDto(OperationTypeId id, DateTime duration, Name name, Description description, List<string> neededSpecializations, Boolean active)
-    {
-        Id = id;
-        Duration = duration;
-        Name = name;
-        Description = description;
-        NeededSpecializations = neededSpecializations;
-        Active = active;
+        public OperationTypeDto(OperationType operationType) : this(operationType.Id, operationType.Duration, operationType.Name, operationType.Description, operationType.NeededSpecializations, operationType.Active) { }
+        
+        public OperationTypeDto(OperationTypeId id, DateTime duration, Name name, Description description,
+            List<string> neededSpecializations, Boolean active) {
+            Id = id;
+            Duration = duration;
+            Name = name;
+            Description = description;
+            NeededSpecializations = neededSpecializations;
+            Active = active;
+        }
     }
 }
