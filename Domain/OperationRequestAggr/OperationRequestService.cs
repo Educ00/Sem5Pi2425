21d6bb171871ac64bef31c325d5deb73fe135a66
BiaMorais30 <127674486+biamorais3@users.noreply.github.com>
2024-10-21 21:26:53 +0100

    US 5.1.16 updated method to create an operation request

--- Domain/OperationRequestAggr/OperationRequestService.cs
+++ Domain/OperationRequestAggr/OperationRequestService.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Sem5Pi2425.Domain.AppointmentAggr;
+using Sem5Pi2425.Domain.OperationTypeAggr;
 using Sem5Pi2425.Domain.PatientAggr;
 using Sem5Pi2425.Domain.Shared;
-using Sem5Pi2425.Domain.StaffAggr;
 using Sem5Pi2425.Domain.SystemUserAggr;
 
 namespace Sem5Pi2425.Domain.OperationRequestAggr
@@ -15,18 +15,20 @@ namespace Sem5Pi2425.Domain.OperationRequestAggr
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IOperationRequestRepository _repo;
-        private readonly StaffService _staffService;
-        private readonly UserService _userService;
         private readonly IAppointmentRepository _appointmentRepo;
+        private readonly IOperationTypeRepository _operationTypeRepo;
+        private readonly IPatientRepository _patientRepo;
+        private readonly IUserRepository _userRepo;
 
-        public OperationRequestService(IUnitOfWork unitOfWork, IOperationRequestRepository repo, StaffService staffService, 
-            UserService userService, IAppointmentRepository appointmentRepo)
+        public OperationRequestService(IUnitOfWork unitOfWork, IOperationRequestRepository repo, IAppointmentRepository appointmentRepo, 
+            IOperationTypeRepository operationTypeRepo, IPatientRepository patientRepo, IUserRepository userRepo)
         {
             _unitOfWork = unitOfWork;
             _repo = repo;
-            _staffService = staffService;
-            _userService = userService;
             _appointmentRepo = appointmentRepo;
+            _operationTypeRepo = operationTypeRepo;
+            _patientRepo = patientRepo;
+            _userRepo = userRepo;
         }
 
         
@@ -49,21 +51,41 @@ namespace Sem5Pi2425.Domain.OperationRequestAggr
             return operationRequest == null ? null : new OperationRequestDTO(operationRequest);
         }
         
-        public async Task<ActionResult<OperationRequestDTO>> AddOperationRequestAsync(OperationRequestDTO dto, StaffDTO doctorStaff) {
-            var operationRequest = new OperationRequest(dto.Deadline, dto.Priority, dto.Doctor, dto.Patient, dto.OperationType);
+        public async Task<ActionResult<OperationRequestDTO>> AddOperationRequestAsync(CreateOperationRequestDto dto, string email)
+        {
+            var deadlineString = dto.Deadline;
+            DateOnly deadline = DateOnly.Parse(deadlineString);
+
+            Enum.TryParse(dto.Priority, true, out Priority priority);
             
-            if (operationRequest.OperationType.NeededSpecializations.Contains(doctorStaff.Specialization.ToString()))
-            {
-                await _repo.AddAsync(operationRequest);
-                await _unitOfWork.CommitAsync();
-                return new OperationRequestDTO(operationRequest.Id, operationRequest.Deadline, operationRequest.Priority, 
-                    operationRequest.Doctor, operationRequest.Patient, operationRequest.OperationType);
+            string[] patientString = dto.Patient.Split(',');
+            string[] operationTypeString = dto.Patient.Split(',');
+
+            var doctor = await _userRepo.GetByEmailAsync(email);
+            var patient = await _patientRepo.GetByIdAsync(new UserId(patientString[0]));
+            var operationType = await _operationTypeRepo.GetByIdAsync(new OperationTypeId(operationTypeString[0]));
+
+            if (doctor == null) {
+                throw new Exception("Doctor not found");
             }
-            return null;
+            if (patient == null) {
+                throw new Exception("Patient not found");
+            }
+            if (operationType == null) {
+                throw new Exception("Operation type not found");
+            }
+
+            var operationRequest = new OperationRequest(deadline,priority, doctor, patient, operationType);
+
+            await _repo.AddAsync(operationRequest);
+            await _unitOfWork.CommitAsync();
+
+            return new OperationRequestDTO(operationRequest.Id, operationRequest.Deadline, operationRequest.Priority,
+                operationRequest.Doctor, operationRequest.Patient, operationRequest.OperationType);
+
         }
         
         public async Task<ActionResult<OperationRequestDTO>> DeleteOperationRequestAsync(OperationRequestId operationRequestId) {
-            Console.WriteLine("OPERATIONREQUESTID SERVICE->" + operationRequestId.Value);
             var operationRequest = await _repo.GetByIdAsync(operationRequestId);
             var appointments = await _appointmentRepo.GetAllAsync();
 
