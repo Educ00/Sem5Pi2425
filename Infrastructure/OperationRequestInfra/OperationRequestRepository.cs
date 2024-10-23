2f4801a3488cc334aea3f8c7a0dc0f919c2661b4
BiaMorais30 <127674486+biamorais3@users.noreply.github.com>
2024-10-23 09:04:55 +0100

    added Include methods in operation request repository

--- Infrastructure/OperationRequestInfra/OperationRequestRepository.cs
+++ Infrastructure/OperationRequestInfra/OperationRequestRepository.cs
@@ -1,9 +1,40 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Sem5Pi2425.Domain.OperationRequestAggr;
 using Sem5Pi2425.Infrastructure.Shared;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sem5Pi2425.Domain.SystemUserAggr;
 
 namespace Sem5Pi2425.Infrastructure.OperationRequestInfra;
 
 public class OperationRequestRepository : BaseRepository<OperationRequest, OperationRequestId>, IOperationRequestRepository {
     public OperationRequestRepository(Sem5Pi2425DbContext context) : base(context.OperationRequests) { }
+    
+    
+    public new async Task<List<OperationRequest>> GetAllAsync() {
+        return await this.Objs
+            .Include(p => p.Doctor)
+            .Include(p => p.Patient)
+            .Include(p => p.OperationType)            
+            .ToListAsync();
+    }
+
+    public new async Task<OperationRequest> GetByIdAsync(OperationRequestId id) {
+        return await this.Objs
+            .Include(p => p.Doctor)
+            .Include(p => p.Patient)
+            .Include(p => p.OperationType)
+            .FirstOrDefaultAsync(x => id.Equals(x.Id));
+    }
+
+    public new async Task<List<OperationRequest>> GetByIdsAsync(List<OperationRequestId> ids) {
+        return await this.Objs
+            .Include(p => p.Doctor)
+            .Include(p => p.Patient)
+            .Include(p => p.OperationType)            
+            .Where(x => ids.Contains(x.Id))
+            .ToListAsync();
+    }
+    
 }
